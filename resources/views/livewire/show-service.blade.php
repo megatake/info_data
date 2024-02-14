c68d66b0a97c1a71753cee30a02d247e504cc6ec
megatake <109475571+megatake@users.noreply.github.com>
2024-02-14 15:22:45 -0400

    #4 Show Services

--- resources/views/livewire/show-service.blade.php
+++ resources/views/livewire/show-service.blade.php
@@ -1,18 +1,17 @@
 <main>
-    <section class="banner bg-tertiary position-relative overflow-hidden">
+    <section class="page-header bg-tertiary">
         <div class="container">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <div class="block text-center text-lg-start pe-0 pe-xl-5">
-                        <h1 class="text-capitalize mb-4">Innovate. Excel. Succeed!</h1>
-                        <p class="mb-4">Unlocking Potential, Igniting Excellence</p> <a type="button" class="btn btn-primary" href="#" data-bs-toggle="modal" data-bs-target="#applyLoan">See
-                            More<span style="font-size: 14px;" class="ms-2 fas fa-arrow-right"></span></a>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="ps-lg-5 text-center">
-                        <img loading="lazy" decoding="async" src="{{ asset('front/images/about-us.png') }}" alt="banner image" class="w-100">
-                    </div>
+            <div class="row">
+                <div class="col-8 mx-auto text-center">
+                    <h2 class="mb-3 text-capitalize">{{ $service->title }}</h2>
+                    <ul class="list-inline breadcrumbs text-capitalize" style="font-weight:500">
+                        <li class="list-inline-item"><a wire:navigate href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="list-inline-item">/ &nbsp; <a wire:navigate href="{{ route('servicesPage') }}">Services</a>
+                        </li>
+                        <li class="list-inline-item">/ &nbsp; {{ $service->title }}
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -63,28 +62,15 @@
         </div>
     </section>
 
-    <section class="section">
+    <section class="section-sm">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="section-title pt-4">
-                        <p class="text-primary text-uppercase fw-bold mb-3">Our Services</p>
-                        <h1>Our online services</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipreiscing elit. Lacus penatibus tincidunt</p>
+            <div class="row g-5">
+
+                <div class="col-lg-12">
+                    <div class="content">
+                        {!! $service->description !!}
                     </div>
                 </div>
-
-                @if ($services->isNotEmpty())
-                    @php
-                        $x = 1;
-                    @endphp
-                @endif
-                @foreach ($services as $service)
-                    <x-service-card :service="$service" :x="$x" />
-                    @php
-                        $x++;
-                    @endphp
-                @endforeach
             </div>
         </div>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\ShowHome;
+use App\Livewire\ShowService;
+use App\Livewire\ShowServicePage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', ShowHome::class)->name('home');
+Route::get('/services', ShowServicePage::class)->name('servicesPage');
+Route::get('/service/{id}', ShowService::class)->name('servicePage');
