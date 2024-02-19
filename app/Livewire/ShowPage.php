c0af55006eb66995da8032bd447b10bea34a9629
megatake <109475571+megatake@users.noreply.github.com>
2024-02-19 12:56:38 -0400

    #14 Show Static Page

---
+++ app/Livewire/ShowPage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Page;
+use Livewire\Component;
+
+class ShowPage extends Component
+{
+
+    public $pageId = null;
+
+    public function mount($id)
+    {
+        $this->pageId = $id;
+    }
+
+    public function render()
+    {
+        $page = Page::findOrfail($this->pageId);
+        return view('livewire.show-page', [
+            'page' => $page
+        ]);
+    }
+}
